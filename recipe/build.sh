4cb9f803b9ed92624439d36aa1b1a6e44b5317de
ocefpaf <ocefpaf@gmail.com>
2016-06-20 09:35:57 -0300

    Build shared and static

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,9 +10,24 @@ export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
 
 # Build static.
+mkdir build_static && cd build_static
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
       -D BUILD_SHARED_LIBS=OFF \
+      $SRC_DIR
+
+make
+# ctest  # Run only for the shared lib build to save time.
+make install
+
+make clean
+
+cd ..
+mkdir build_shared && cd build_shared
+# Build shared.
+cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
+      -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
+      -D BUILD_SHARED_LIBS=ON \
       $SRC_DIR
 
 make
