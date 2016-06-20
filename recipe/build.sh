8c25f00bcd9273264f0035c5c744ca714390a0db
Kai Muehlbauer <kai.muehlbauer@uni-bonn.de>
2016-06-20 09:25:10 +0200

    WIP: fix rpath issue

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+if [[ $(uname) == Darwin ]]; then
+  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
+elif [[ $(uname) == Linux ]]; then
+  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+fi
+
+export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
+
 # Build static.
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D CMAKE_INSTALL_LIBDIR:PATH=$PREFIX/lib \
       -D BUILD_SHARED_LIBS=OFF \
       $SRC_DIR
+
 make
 ctest
 make install
