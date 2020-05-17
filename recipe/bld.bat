f188ef6b6ddaa583f34f99bd2ca62efafbd168f3
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-05-17 14:08:20 +0200

    Specify generators and build type

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,9 +3,9 @@ set "cwd=%cd%"
 set "LIBRARY_PREFIX=%LIBRARY_PREFIX:\=/%"
 set "MINGWBIN=%LIBRARY_PREFIX%/mingw-w64/bin"
 
-
-set "LDFLAGS=%LDFLAGS% -L%LIBRARY_PREFIX%/lib -Wl,-rpath,%LIBRARY_PREFIX%/lib -lcurl -lhdf5 -lhdf5_hl -ldf -lmfhdf"
-set "CFLAGS=%CFLAGS% -fPIC -I%LIBRARY_PREFIX%/include"
+:: These flags cause errors during CMake; disable for now?
+REM set "LDFLAGS=%LDFLAGS% -L%LIBRARY_PREFIX%/lib -Wl,-rpath,%LIBRARY_PREFIX%/lib -lcurl -lhdf5 -lhdf5_hl -ldf -lmfhdf"
+REM set "CFLAGS=%CFLAGS% -fPIC -I%LIBRARY_PREFIX%/include"
 :: PENDING -> PARALLEL MPI STUFF
 set PARALLEL=""
 
@@ -15,27 +15,29 @@ set PARALLEL=""
 :: and general chaos in the builds here. An attempt at patching upstream was
 :: made, but getting that working fully and robustly appears difficult. This
 :: should just work all the time.
-rmdir "%PREFIX%\lib\cmake\netCDF\*" /s /q
+rmdir "%LIBRARY_LIB%\cmake\netCDF" /s /q
 
 :: Build static.
 rmdir build_static /s /q
 mkdir build_static
 cd build_static
-cmake -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+cmake -G "Unix Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_LIBDIR:PATH=%LIBRARY_PREFIX%/lib ^
       -D BUILD_SHARED_LIBS=OFF ^
-      -D CMAKE_C_COMPILER=%MINGWBIN%/gcc.exe ^
-      -D CMAKE_Fortran_COMPILER=%MINGWBIN%/gfortran.exe ^
+      -D CMAKE_C_COMPILER:PATH=%MINGWBIN%/gcc.exe ^
+      -D CMAKE_Fortran_COMPILER:PATH=%MINGWBIN%/gfortran.exe ^
       %PARALLEL% ^
       %SRC_DIR%
-if errorlevel exit 1
+if errorlevel 1 exit 1
 
 make
-if errorlevel exit 1
+if errorlevel 1 exit 1
 :: ctest
-:: if errorlevel exit 1
+:: if errorlevel 1 exit 1
 make install
-if errorlevel exit 1
+if errorlevel 1 exit 1
 
 
 make clean
@@ -45,19 +47,21 @@ cd ..
 rmdir build_shared /s /q
 mkdir build_shared
 cd build_shared
-cmake -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+cmake -G "Unix Makefiles" ^
+      -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_LIBDIR:PATH=%LIBRARY_PREFIX%/lib ^
       -D BUILD_SHARED_LIBS=ON ^
       -D CMAKE_C_COMPILER=%MINGWBIN%/gcc.exe ^
       -D CMAKE_Fortran_COMPILER=%MINGWBIN%/gfortran.exe ^
       %SRC_DIR%
-if errorlevel exit 1
+if errorlevel 1 exit 1
 
 make
-if errorlevel exit 1
+if errorlevel 1 exit 1
 ctest
-if errorlevel exit 1
+if errorlevel 1 exit 1
 make install
-if errorlevel exit 1
+if errorlevel 1 exit 1
 
 cd %cwd%
