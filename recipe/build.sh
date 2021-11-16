f70a114fea605183635018c5720554fc837ea3b3
Isuru Fernando <isuruf@gmail.com>
2021-11-16 10:03:57 -0600

    cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,15 +12,9 @@ else
   PARALLEL=""
 fi
 
-if [[ $(uname) == Darwin ]]; then
-  export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-elif [[ $(uname) == Linux ]]; then
-  export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
-fi
-
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib -lcurl -lhdf5 -lhdf5_hl -ldf -lmfhdf"
 export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
-if [[ $(uname) == Darwin ]] && [[ "${CC}" != "clang" ]]; then
+if [[ "$target_platform" == osx-* ]]; then
   export FFLAGS="-isysroot $CONDA_BUILD_SYSROOT $FFLAGS"
 fi
 
@@ -54,11 +48,7 @@ cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
       -D BUILD_SHARED_LIBS=ON \
       $SRC_DIR
 
-make
-if [[ $(uname) == Linux ]]; then
-  # seems to be needed to find libquadmath.so.0
-  export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
-fi
+make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
   ctest -VV
 fi
